a5b8d9f643521ae8a3eb95f1f8d49d30fd47e9be
Omar McIver <omar.mciver@mode3cloud.com>
2024-02-05 22:23:23 -0600

    Update docker-compose.yml and settings.yaml

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Setting up
+echo "Setting up..."
+.venv/bin/python scripts/setup
+
 # Ingest files
 echo "Ingesting files..."
 .venv/bin/python scripts/ingest_folder.py /ingest_docs
